6d7bd56d68bc9a3fc6019f42f116cb5ee0855bfd
Mide <ibrahimayomide330@gmail.com>
2023-10-20 22:53:49 +0100

    feat: add placedetail in url pattern

--- cleanbites_api/urls.py
+++ cleanbites_api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from . import views
 
 # URLConf
-urlpatterns = [path("places/", views.get_places)]
+urlpatterns = [path("places/", views.get_places),
+               path('places/<int:id>/', views.place_detail)]
