973c68d1d4a8c3607f0cd85cdb4a46a6e82aaf87
Mide <ibrahimayomide330@gmail.com>
2023-10-26 15:33:13 +0100

    feat: register admin and superuser

--- cleanbites_api/admin.py
+++ cleanbites_api/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from . import models
 
 # Register your models here.
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "password1", "password2", "email", "first_name", "last_name"),
+            },
+        ),
+    )
+
+
+@admin.register(models.PlaceDetail)
+class PlaceDetailAdmin(admin.ModelAdmin):
+    list_display = ['business_name', 'hygiene_score']
+    list_per_page = 25
+    search_fields = ['business_name']
+
+
+admin.site.register(models.UserFavorite)
+admin.site.register(models.PlaceReview)
+
