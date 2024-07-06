26f3a7ff0dadf699d93a29b7fe3de683a890c4c7
Mide Ibrahim <mide@Mides-MacBook-Pro.local>
2024-07-06 15:26:27 +0100

    update allowed hosts

--- cleanbites/settings.py
+++ cleanbites/settings.py
@@ -20,7 +20,9 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cleanbites_backend.settings')
 
+# application = get_wsgi_application()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -30,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['cleanbites-backend.onrender.com']
 
 
 # Application definition
